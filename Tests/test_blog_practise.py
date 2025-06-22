2b554bbc8b4728ca852e81f5c995923a19213460
mahesh <mail2mahebabu@gmail.com>
2025-06-22 23:39:58 +0530

    change POM-test_blog.py tests-test_blog_practise.py

--- Tests/test_blog_practise.py
+++ Tests/test_blog_practise.py
@@ -1,32 +1,32 @@
-# import pytest
-#
-# from POM.test_blog import test
-# from time import sleep
-#
-# @pytest.mark.blog
-# def test_blog(tes):
-#     t=test(tes)
-#     # t.det("Mahesh Babu",'maheshrony@gmail.com','8639563301','Nandyala')
-#     # t.drop_down()
-#     # t.calender()
-#     # t.switch_window()
-#     # sleep(2)
-#     # t.web()
-#     # sleep(2)
-#     # t.condition_based_data()
-#     # sleep(1)
-#     # t.chek()
-#     # sleep(1)
-#     t.pops('HOBBIT')
-#     t.ACTIONS()
-#     t.fames('mahesh@gmail.com','test123','test123')
-#
 import pytest
 
-from POM.practise import practise
+from POM.test_blog import test
 from time import sleep
 
-def test_check(b):
-    p=practise(b)
-    p.prac("mahesh",'mahesh@gmail;.com',"8639563301","13/Nov/2026","mahesh")
+@pytest.mark.blog
+def test_blog(tes):
+    t=test(tes)
+    t.det("Mahesh Babu",'maheshrony@gmail.com','8639563301','Nandyala')
+    # t.drop_down()
+    # t.calender()
+    # t.switch_window()
+    # sleep(2)
+    # t.web()
+    # sleep(2)
+    # t.condition_based_data()
+    # sleep(1)
+    # t.chek()
+    # sleep(1)
+    # t.pops('HOBBIT')
+    # t.ACTIONS()
+    # t.fames('mahesh@gmail.com','test123','test123')
+
+import pytest
+
+# from POM.practise import practise
+# from time import sleep
+#
+# def test_check(b):
+#     p=practise(b)
+#     p.prac("mahesh",'mahesh@gmail;.com',"8639563301","13/Nov/2026","mahesh")
 
