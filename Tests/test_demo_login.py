78db8b4dc1c550f05664b292bbb77f72fa5a581e
mahesh <mail2mahebabu@gmail.com>
2025-06-22 23:18:35 +0530

    commented demo_login

--- Tests/test_demo_login.py
+++ Tests/test_demo_login.py
@@ -1,19 +1,19 @@
-import allure
-import pytest
-from allure_commons.types import AttachmentType
-from POM.demo_login import login
-from Utilities.Excelreader import reader
-
-cred_details = reader('Test_data.xlsx', 'login')
-
-
-@pytest.mark.login
-@pytest.mark.parametrize('username,password', cred_details)
-def test_demo_login(demo,username, password):
-    l = login(demo)
-    l.log(username, password)
-    condition = demo.find_element("link text", "Log out").is_displayed()
-    print(condition)
-    if condition == False:
-        allure.attach(demo.get_screenshot_as_png(), name="test_demo_login.png", attachment_type=AttachmentType.PNG)
-    assert condition
+# import allure
+# import pytest
+# from allure_commons.types import AttachmentType
+# from POM.demo_login import login
+# from Utilities.Excelreader import reader
+#
+# cred_details = reader('Test_data.xlsx', 'login')
+#
+#
+# @pytest.mark.login
+# @pytest.mark.parametrize('username,password', cred_details)
+# def test_demo_login(demo,username, password):
+#     l = login(demo)
+#     l.log(username, password)
+#     condition = demo.find_element("link text", "Log out").is_displayed()
+#     print(condition)
+#     if condition == False:
+#         allure.attach(demo.get_screenshot_as_png(), name="test_demo_login.png", attachment_type=AttachmentType.PNG)
+#     assert condition
